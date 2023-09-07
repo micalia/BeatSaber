abc85c3d52393448f52512b094702c6df5c80c45
micalia <micalia0612@gmail.com>
2023-09-07 13:58:09 +0900

    C++ 블럭 노드 생성완료

---
+++ Source/BeatSaber/Private/NodeBlock.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NodeBlock.h"
+#include "ProceduralMeshComponent.h"
+#include <Components/StaticMeshComponent.h>
+
+// Sets default values
+ANodeBlock::ANodeBlock()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	rootComp = CreateDefaultSubobject<USceneComponent>(TEXT("rootComp"));
+	SetRootComponent(rootComp);
+
+	proceduralMesh = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("proceduralMesh"));
+	proceduralMesh->SetupAttachment(rootComp);
+	proceduralMesh->SetRelativeLocation(FVector(0, 0, -100));
+	proceduralMesh->SetRelativeRotation(FRotator(0, 90, 0));
+	proceduralMesh->SetCollisionProfileName(TEXT("BlockAll"));
+	proceduralMesh->bUseComplexAsSimpleCollision = false;
+
+	sm_nodeBlock = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("sm_nodeBlock"));
+	sm_nodeBlock->SetupAttachment(rootComp);
+	sm_nodeBlock->SetRelativeLocation(FVector(0, 0, -100));
+	sm_nodeBlock->SetRelativeRotation(FRotator(0, 90, 0));
+	sm_nodeBlock->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	sm_nodeBlock->bHiddenInGame = true;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> tempNodeBlockMesh(TEXT("/Script/Engine.StaticMesh'/Game/Models/RedNoteBlock.RedNoteBlock'"));
+	if (tempNodeBlockMesh.Succeeded()) {
+		sm_nodeBlock->SetStaticMesh(tempNodeBlockMesh.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void ANodeBlock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANodeBlock::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/BeatSaber/Public/NodeBlock.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NodeBlock.generated.h"
+
+UCLASS()
+class BEATSABER_API ANodeBlock : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ANodeBlock();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(EditAnywhere)
+	class USceneComponent* rootComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	class UProceduralMeshComponent* proceduralMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	class UStaticMeshComponent* sm_nodeBlock;
+};
