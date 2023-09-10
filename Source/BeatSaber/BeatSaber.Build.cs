9c88336b9b7094c293c5b833df2665cf4182b4f4
micalia <micalia0612@gmail.com>
2023-09-10 09:51:34 +0900

    장애물 벽 및 가시공 에셋 추가 완료

--- Source/BeatSaber/BeatSaber.Build.cs
+++ Source/BeatSaber/BeatSaber.Build.cs
@@ -8,7 +8,7 @@ public class BeatSaber : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "UMG", "ProceduralMeshComponent" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "UMG", "ProceduralMeshComponent", "OpenXRHMD" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
