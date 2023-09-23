f954f74e286b466692d9a6f928f5c558804b742b
EoJaeGyeng <amzi1118@gmail.com>
2023-09-24 01:05:29 +0900

    Add editor map & making editor UI

---
+++ Source/BeatSaber/Public/EO_EditorWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EO_EditorWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEATSABER_API UEO_EditorWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
